0d811ceeaf85a79c83121b2352b769caeb7716cc
Michael Colon <michaelcolon@youaredreaminginspacewheretimeisjustenergy.local>
2016-07-25 13:58:40 -0400

    added error validation for raw_input

--- Interface.py
+++ Interface.py
@@ -26,10 +26,15 @@ class Interface(object):
         excluding = "excluding "
         for n in ticket.numbers:
           e = e+str(n)+", "
-      number = input("Select {0} # (1 thru 69 {1} {2}): ".format(self.positional[len(ticket.numbers)], excluding, e))
-      ticket.addNumber(number)
+      number = raw_input("Select {0} # (1 thru 69 {1} {2}): ".format(self.positional[len(ticket.numbers)], excluding, e))
+      if not number:
+        print ("please enter a number")
+      else:
+        number = int(number)
+      if (number):
+          ticket.addNumber(number)
     elif ticket.powerball == None or ticket.powerball == 0:
-      number = input("Select Powerball # (1 thru 26): ")
+      number = int(raw_input("Select Powerball # (1 thru 26): "))
       ticket.addPowerball(number)
 
 if __name__ == '__main__':
